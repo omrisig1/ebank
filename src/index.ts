93a6a2f3a21b3025b247fe5ac4c792a03803d326
Omri <omrisig1@gmail.com>
2022-02-02 18:01:58 +0200

    added config json file for constants

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import log from '@ajar/marker';
 import { multiplyR } from './calc.js';
 import saySomething from './myModule.js';
+import config from '../config.json';
+
+const conf = JSON.parse(JSON.stringify(config));
+console.log(conf.test);
+console.log(conf.scripts.one);
 
 const multiplied = multiplyR(3, 4, 5);
 console.log(multiplied);
