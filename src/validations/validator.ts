e66e4220dcdfd5f43b6bc683f6fa8b6e5ec6b3c7
Omri <omrisig1@gmail.com>
2022-02-03 20:55:51 +0200

    validations

---
+++ src/validations/validator.ts
@@ -0,0 +1,50 @@
+import config from '../../config.json';
+
+export function stringNotEmpty(str: any) {
+  if (str && typeof str === 'string' && str !== '') {
+    return true;
+  }
+  throw new Error('value is not a string');
+}
+
+export function mandatoryFieldExists(object: any, fieldName: string) {
+  if (
+    typeof object === 'object' &&
+    object.fieldName &&
+    stringNotEmpty(object[fieldName])
+  ) {
+    return true;
+  }
+  throw new Error('mandatory field missing');
+}
+
+export function isValNumeric(val: any) {
+  if (val && !Number(val)) {
+    return true;
+  }
+  throw new Error('value is not numeric');
+}
+
+export function stringLengthAtLeast(val: any, length: number) {
+  if (stringNotEmpty(val) && val.length >= length) {
+    return true;
+  }
+  throw new Error('string length error');
+}
+
+export function stringLengthEquals(val: any, length: number) {
+  if (stringNotEmpty(val) && val.length === length) {
+    return true;
+  }
+  throw new Error('string length error');
+}
+
+export function transferSizeSmallerThan(accountType: string, amount: number) {
+  if (
+    isValNumeric(amount) &&
+    Number(config.individual_account_minimum_transfer) <= Number(amount)
+  ) {
+    return true;
+  }
+  throw new Error('trasnfer amount exceeds account type permition error');
+}
