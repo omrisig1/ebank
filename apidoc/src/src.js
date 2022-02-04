982ea77d136c8d756cca1a265ce498fd53d9128c
Omri <omrisig1@gmail.com>
2022-02-04 22:09:17 +0200

    basic apidoc ready

---
+++ apidoc/src/src.js
@@ -0,0 +1,37 @@
+/**
+ * @api {get} /individual/:id Request individual account information
+ * @apiName GetIndividualAccountInformation
+ * @apiVersion 1.0.0
+ * @apiGroup Individual
+ * @apiParam {Number} id Account unique ID3.
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ */
+
+/**
+ * @api {get} /buisness/:id Request buisness account information
+ * @apiName GetBuisnessAccountInformation
+ * @apiVersion 1.0.0
+ * @apiGroup Buisness
+ * @apiParam {Number} id Account unique ID2.
+ * @apiSuccess {String} company_name company name of the account.
+ */
+
+/**
+ * @api {post} /individual/ Create Individual Account
+ * @apiName CreateIndividualAccount
+ * @apiVersion 1.0.0
+ * @apiGroup Individual
+ * @apiParam {Number} id Account unique ID2.
+ * @apiSuccess {String} company_name company name of the account.
+ */
+
+/**
+ * @api {post} /family/ Create Family Account
+ * @apiName CreateFamilyAccount
+ * @apiVersion 1.0.0
+ * @apiGroup Family
+ * @apiParam {Number} id Account unique ID2.
+ * @apiSuccess {String} company_name company name of the account.
+ */
