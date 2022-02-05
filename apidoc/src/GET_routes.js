79f9031cd1fdc235d39b8a6b2a6c0ef44a791df9
Omri <omrisig1@gmail.com>
2022-02-05 14:07:28 +0200

    apidoc added diferent routes and errors and success response

--- apidoc/src/GET_routes.js
+++ apidoc/src/GET_routes.js
@@ -3,10 +3,22 @@
  * @apiName GetIndividualAccountInformation
  * @apiVersion 1.0.0
  * @apiGroup Individual
+ * @apiError Individual The <code>id</code> of the Account was not found.
  * @apiParam {Number} id Account unique ID3.
  * @apiSuccess {String} firstname Firstname of the User.
  * @apiSuccess {String} lastname  Lastname of the User.
- *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "Account Not Found"
+ *     }
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "type": "INDIVIDUAL",
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
  */
 
 /**
@@ -14,8 +26,21 @@
  * @apiName GetBuisnessAccountInformation
  * @apiVersion 1.0.0
  * @apiGroup Buisness
+ * @apiError Buisness_ID_Not_Found The <code>id</code> of the Account was not found.
  * @apiParam {Number} id Account unique ID2.
  * @apiSuccess {String} company_name company name of the account.
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "Account Not Found"
+ *     }
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "type": "BUISNESS",
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
  */
 
 /**
