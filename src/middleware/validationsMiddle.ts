99315a6799be4a76bc4268999b56f1828de0bb27
Omri <omrisig1@gmail.com>
2022-02-06 20:21:26 +0200

    aaa

--- src/middleware/validationsMiddle.ts
+++ src/middleware/validationsMiddle.ts
@@ -1,6 +1,6 @@
 import { Response, Request, NextFunction } from 'express';
-import * as Validator from '../validations/validator.js';
-import config from '../../config.json';
+// import * as Validator from '../validations/validator.js';
+// import config from '../../config.json';
 
 export function minBalanceFromIndividTrans(req: Request, res: Response, next: NextFunction) : void{
   try {
