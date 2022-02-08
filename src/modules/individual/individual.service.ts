358a10c9fcedca7001c40f61c9246577457abe57
Omri <omrisig1@gmail.com>
2022-02-08 13:32:32 +0200

    service done with transaction

--- src/modules/individual/individual.service.ts
+++ src/modules/individual/individual.service.ts
@@ -6,6 +6,7 @@ import * as dal from "./individual.dal.js";
 import * as util from "../utils.dal.js";
 import * as Validator from "../../validations/validator.js";
 import { IChangeStatus } from "../../types/types";
+import { connection as db } from '../../db/mysql.connection.js';
 
 // Create an individual account
 export async function createNewIndividualAccount(payload: IIndividualAccount): Promise<any> {
@@ -29,9 +30,15 @@ export async function getIndividualAccountByAccountId(idToRead: number): Promise
 export async function changeAccountStatus(payload: IChangeStatus): Promise<any> {
     // TODO: call dal to create new individual account
     //       add validations and business logic
-
-    const accounts = await util.getAccountsByIds(payload.list_of_accounts);
-    Validator.NumberEquals(accounts.length, payload.list_of_accounts.length);
-    const accounts_statuses = await util.changeAccountStatus(payload.list_of_accounts, payload.action);
-    return accounts_statuses;
+    await db.beginTransaction();
+    try{
+        const accounts = await util.getAccountsByIds(payload.list_of_accounts);
+        Validator.NumberEquals(accounts.length, payload.list_of_accounts.length);
+        const accounts_statuses = await util.changeAccountStatus(payload.list_of_accounts, payload.action);
+        await db.commit();
+        return accounts_statuses;
+    }
+    catch(err){
+        await db.rollback();
+    }
 }
