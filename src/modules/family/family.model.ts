10640ae1b3ac68c1e8298662e27259e9a91ccc12
Noam Shneor <noams@rapyd.ac>
2022-02-09 16:35:30 +0200

    fix family dal - add transactions

--- src/modules/family/family.model.ts
+++ src/modules/family/family.model.ts
@@ -1,6 +1,6 @@
-// import IAccount from '../account.model';
-// import { IIndividualAccount } from '../individual/individual.model.js';
-// export interface IFamilyAccount extends IAccount {
-//   context?: string;
-//   owners?: string[] | IIndividualAccount[];
-// }
+import IAccount from '../account.model';
+import { IIndividualAccount } from '../individual/individual.model.js';
+export interface IFamilyAccount extends IAccount {
+  context?: string;
+  owners?: string[] | IIndividualAccount[];
+}
--- src/modules/utils.dal.ts
+++ src/modules/utils.dal.ts
@@ -51,25 +51,38 @@ export async function updateBalance(account_id: number, balance: number): Promis
   return account;
 }
 
-
-export async function multiTransfer(transfers : simple_transfer[]) : Promise<IAccount[]>{
-  let accounts :  IAccount[] = [];
+export async function multiTransfer(transfers: simple_transfer[]): Promise<IAccount[]> {
+  let accounts: IAccount[] = [];
   await db.beginTransaction();
-    try{
-      for (const transfer of transfers) {
-        accounts.push(await updateBalance(transfer.account_id,transfer.new_balance));
-      }
-      await db.commit();
-      return accounts;
+  try {
+    for (const transfer of transfers) {
+      accounts.push(await updateBalance(transfer.account_id, transfer.new_balance));
     }
-    catch(err) {
-      await db.rollback();
-      throw err;
-    }
-    
-
+    await db.commit();
+    return accounts;
+  } catch (err) {
+    await db.rollback();
+    throw err;
+  }
 }
 
+export function createFamilyIndividualArray(
+  individuals_new_balance: [string, string][],
+  account_id: number
+): [family_account_id: string, individual_account_id: string][] {
+  let family_account_owners: [string, string][] = [];
+  for (const owner of individuals_new_balance) {
+    family_account_owners.push([String(account_id), owner[0]]);
+  }
+  return family_account_owners;
+}
 
-
-
+export function getAccountsIdsArray(
+  account_balance: [account_id: string, balance: string][]
+): string[] {
+  const accounts: string[] = [];
+  for (const account of account_balance) {
+    accounts.push(account[0]);
+  }
+  return accounts;
+}
