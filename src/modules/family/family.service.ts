9e7ee4d1fb90a7fff721fe2359c5003e7e79d7b4
Noam Shneor <noams@rapyd.ac>
2022-02-09 09:28:29 +0200

    close family function

--- src/modules/family/family.service.ts
+++ src/modules/family/family.service.ts
@@ -1,8 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-import { IAddIndividualsToFamily, IRemoveIndividualsToFamily, ITransfer } from "../../types/types.js";
-import * as dal from "./family.dal.js";
+import {
+  IAddIndividualsToFamily,
+  IRemoveIndividualsToFamily,
+  ITransfer,
+} from '../../types/types.js';
+import { changeAccountStatus } from '../utils.dal.js';
+import * as dal from './family.dal.js';
+import { IFamilyAccount } from './family.model.js';
 
 // Create family account
 // export async function createNewFamilyAccount(payload: ICreateFamilyAccount): Promise<any> {
@@ -14,49 +20,59 @@ import * as dal from "./family.dal.js";
 // }
 
 // Get family account by ID - FULL/SHORT
-export async function getFamilyAccountById(family_id: number, details_level: string = "full"): Promise<any> {
-    // TODO: call dal to get family account
-    //       add validations and business logic
-    //       and return family model by the details_level
-    
-    const family_account_full = await dal.getFamilyAccountById(family_id, details_level);
-    return family_account_full;
+export async function getFamilyAccountById(
+  family_id: number,
+  details_level: string = 'full'
+): Promise<any> {
+  // TODO: call dal to get family account
+  //       add validations and business logic
+  //       and return family model by the details_level
+
+  const family_account_full = await dal.getFamilyAccountById(family_id, details_level);
+  return family_account_full;
 }
 
 // Add individuals to family account - return FULL/SHORT
-export async function addIndividualsToFamily(family_id: number, details_level: string = "full", payload: IAddIndividualsToFamily): Promise<any> {
-    // TODO: call dal to add individuals to family account
-    //       add validations and business logic
-    //       and return family model by the details_level
+export async function addIndividualsToFamily(
+  family_id: number,
+  details_level: string = 'full',
+  payload: IAddIndividualsToFamily
+): Promise<any> {
+  // TODO: call dal to add individuals to family account
+  //       add validations and business logic
+  //       and return family model by the details_level
 
-    const family_account = await dal.addIndividualsToFamily(family_id, details_level, payload);
-    return family_account;
+  const family_account = await dal.addIndividualsToFamily(family_id, details_level, payload);
+  return family_account;
 }
 
 // Delete individuals from family account - return FULL/SHORT
-export async function deleteIndividualsFromFamily(family_id: number, details_level: string = "full", payload: IRemoveIndividualsToFamily): Promise<any> {
-    // TODO: call dal to delete individuals from family account
-    //       add validations and business logic
-    //       and return family model by the details_level
-    
-    const family_account = await dal.deleteIndividualsFromFamily(family_id, details_level, payload);
-    return family_account;
+export async function deleteIndividualsFromFamily(
+  family_id: number,
+  details_level: string = 'full',
+  payload: IRemoveIndividualsToFamily
+): Promise<any> {
+  // TODO: call dal to delete individuals from family account
+  //       add validations and business logic
+  //       and return family model by the details_level
+
+  const family_account = await dal.deleteIndividualsFromFamily(family_id, details_level, payload);
+  return family_account;
 }
 
 // Transfer F2B
 export async function transferFromFamilyToBusiness(payload: ITransfer): Promise<any> {
-    // TODO: call dal to transfer money from family to business
-    //       add validations and business logic
+  // TODO: call dal to transfer money from family to business
+  //       add validations and business logic
 
-    const results = await dal.transferFromFamilyToBusiness(payload);
-    return results;
+  const results = await dal.transferFromFamilyToBusiness(payload);
+  return results;
 }
 
 // Close family account by ID
-export async function closeFamilyAccountById(family_id: number): Promise<any> {
-    // TODO: call dal to create new family account
-    //       add validations and business logic
-
-    const results = await dal.closeFamilyAccountById(family_id);
-    return results;
+export async function closeFamilyAccountById(account_id: number): Promise<number | undefined> {
+  const family = await dal.getFamilyAccountByAccountId(account_id, 'short');
+  if (Array.isArray(family.owners) && family.owners.length > 0) return undefined;
+  await changeAccountStatus([account_id.toString()], '2');
+  return account_id;
 }
