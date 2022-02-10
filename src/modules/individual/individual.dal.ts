c18bcf7bf03099c9ef136cfc4b05414f7a546b2e
Roee Sharon <roees@rapyd.ac>
2022-02-10 17:50:43 +0200

    feat: add address model to app
    (updated individual and business models to support it).

--- src/modules/individual/individual.dal.ts
+++ src/modules/individual/individual.dal.ts
@@ -2,11 +2,14 @@ import { IIndividualAccount } from './individual.model.js';
 import { RowDataPacket } from 'mysql2/promise';
 import { connection as db } from '../../db/mysql.connection.js';
 import { createAccount } from '../utils.dal.js';
+import { createAddress } from '../address/address.dal.js';
+import IAddress from '../address/address.model.js';
 
 export async function createIndividualAccount(
   payload: IIndividualAccount
 ): Promise<IIndividualAccount> {
   const account_id = await createAccount(payload);
+  const address_id = await createAddress(payload.address as IAddress);
   const sql2 = 'INSERT INTO IndividualAccounts SET ?;';
   await db.query(sql2, {
     account_id,
@@ -14,7 +17,7 @@ export async function createIndividualAccount(
     first_name: payload.first_name,
     last_name: payload.last_name,
     email: payload.email,
-    address_id: payload.address_id,
+    address_id
   });
   const individual = await getIndividualAccountByAccountId(account_id);
   return individual;
@@ -24,32 +27,51 @@ export async function getIndividualAccountByAccountId(
   account_id: number
 ): Promise<IIndividualAccount> {
   const sql = `SELECT * 
-                FROM Accounts as A JOIN IndividualAccounts as I
-                    ON A.account_id = I.account_id 
+                FROM Accounts as A 
+                JOIN IndividualAccounts as I
+                    ON A.account_id = I.account_id
                 WHERE A.account_id = ?;`;
+  
+  const sql2 = `SELECT Ad.*
+                FROM Addresses as Ad
+                JOIN IndividualAccounts as I
+                    ON I.address_id = Ad.address_id
+                WHERE I.account_id = ?;`
   const [individuals] = (await db.query(sql, account_id)) as RowDataPacket[][];
-  return individuals[0] as IIndividualAccount;
+  let individual_no_address_key = individuals[0];
+  delete individual_no_address_key.address_id;
+
+  const [address] = (await db.query(sql2, account_id)) as RowDataPacket[][];
+  const individual: IIndividualAccount = {
+    ...individual_no_address_key as IIndividualAccount,
+    address: address[0] as IAddress
+  }
+  // return individuals[0] as IIndividualAccount;
+  return individual;
 }
 
-export async function getIndividualsByIndividualsIds(
-  individual_ids: string[]
-): Promise<IIndividualAccount[]> {
-  const sql = `SELECT * 
-                FROM Accounts as A JOIN IndividualAccounts as I
-                    ON A.account_id = I.account_id 
-                WHERE individual_id IN (?);`;
-  const [individuals] = await db.query(sql, [individual_ids]);
-  return individuals as IIndividualAccount[];
-}
 export async function getIndividualsByAccountsIds(
   account_ids: string[] | number[]
 ): Promise<IIndividualAccount[]> {
   if(account_ids.length == 0) {
     return [];
   }
-  const sql = `SELECT * FROM Accounts as A
-             JOIN IndividualAccounts as I ON A.account_id = I.account_id
-              WHERE A.account_id IN (?);`;
-  const [individuals] = await db.query(sql, [account_ids]);
-  return individuals as IIndividualAccount[];
+  let individuals: IIndividualAccount[] = [];
+  for (const account_id of account_ids) {
+    individuals.push(await getIndividualAccountByAccountId(account_id as number));
+  }
+  return individuals;
 }
+
+// export async function getIndividualsByIndividualsIds(
+//   individual_ids: string[]
+// ): Promise<IIndividualAccount[]> {
+//   const sql = `SELECT * 
+//                 FROM Accounts as A 
+//                 JOIN IndividualAccounts as I
+//                     ON A.account_id = I.account_id
+//                 WHERE individual_id IN (?);`;
+//   const [individuals] = await db.query(sql, [individual_ids]);
+//   return individuals as IIndividualAccount[];
+// }
+
--- src/modules/individual/individual.model.ts
+++ src/modules/individual/individual.model.ts
@@ -1,8 +1,9 @@
 import IAccount from "../account.model";
+import IAddress from "../address/address.model";
 export interface IIndividualAccount extends IAccount {
     individual_id: number;
     first_name: string;
     last_name: string;
     email?: string;
-    address_id?: number;
+    address?: IAddress;
 }
