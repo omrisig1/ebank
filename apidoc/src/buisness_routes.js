ec221c26f7f63b873df6b5b7d3d706e298343972
Omri <omrisig1@gmail.com>
2022-02-12 16:20:00 +0200

    apidoc all routes incomplete

--- apidoc/src/buisness_routes.js
+++ apidoc/src/buisness_routes.js
@@ -1,10 +1,10 @@
 /**
- * @api {get} /buisness/:id Request buisness account information
+ * @api {get} /buisness/:id 1.Request buisness account information
  * @apiName GetBuisnessAccountInformation
  * @apiVersion 1.0.0
  * @apiGroup Buisness Account
  * @apiError buisness The <code>id</code> of the Account was not found.
- * @apiParam {Number} primary_id primary id of the account
+ * @apiParam {Number} primary_id primary id of the account 
  * @apiSuccess {number} primary_id .
  * @apiSuccess {string} currency .
  * @apiSuccess {number} balance .
@@ -12,7 +12,11 @@
  * @apiSuccess {number} address_id .
  * @apiSuccess {number} company_id .
  * @apiSuccess {string} company_name .
+ * @apiSuccess {string} e_date .
+ * @apiSuccess {string} a_date .
  * @apiSuccess {string} context .
+ * @apiSuccess {string} black_list .
+
 
  * @apiErrorExample {json} Error-Response:
  *     HTTP/1.1 404 Not Found
@@ -21,8 +25,11 @@
  *     }
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
+ *   "status": 200, 
+ *   "message": "Buisness account Found",
+ *   "data":
  *     {
- *       "primary_id": "123",
+ *       "primary_id": "1234567",
  *       "type": "buisness",
  *       "currency": "USD"",
  *       "balance": 10024.3,
@@ -30,26 +37,27 @@
  *       "address_id": "31",
  *       "company_name": "spid",
  *       "company_id": "88888888"
- *       "context": "suppliers"
+ *       "context": "suppliers",
+ *       "black_list": 0,
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
  *     }
  */
 
 /**
- * @api {post} /buisness/ create buisness account information
+ * @api {post} /buisness/ 2.create buisness account information
  * @apiName CreateBuisnessAccount
  * @apiVersion 1.0.0
  * @apiGroup Buisness Account
  * @apiError Buisness Error creating account for Buisness.
 
- * @apiParam {string} type Primary ID of the User.
- * @apiParam {number} primary_id .
- * @apiParam {string} currency .
- * @apiParam {number} balance .
- * @apiParam {string} status .
- * @apiParam {number} address_id .
- * @apiParam {number} company_id .
- * @apiParam {string} company_name .
- * @apiParam {string} context .
+ * @apiParam {number} primary_id .78 digit bigger than 1000000 number
+ * @apiParam {string} currency . Currency of the Account
+ * @apiParam {number} balance . Initial Balance Of The Account
+ * @apiParam {Object} address . 
+ * @apiParam {number} company_id . 8 digit bigger than 10000000 number
+ * @apiParam {string} company_name . The Comapny Name
+ * @apiParam {string} context . The Context Which The Account Is For.
 
  * @apiSuccess {number} primary_id .
  * @apiSuccess {number} account_id .
@@ -60,6 +68,8 @@
  * @apiSuccess {number} company_id .
  * @apiSuccess {string} company_name .
  * @apiSuccess {string} context .
+ * @apiSuccess {string} black_list .
+
  * @apiErrorExample {json} Error-Response:
  *     HTTP/1.1 404 Not Found
  *     {
@@ -67,23 +77,28 @@
  *     }
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
+ *   "status": 200,
+ *   "message": "Business account created",
+ *   "data":
  *     {
- *       "message": "account created succesfuly",
- *       "primary_id": "123",
+ *       "primary_id": "1234567",
  *       "type": "individual",
  *       "account_id": "1234",
  *       "currency": "USD"",
  *       "balance": 10024.3,
  *       "status": "ACTIVE",
  *       "address_id": "31",
- *       "company_id": "312",
+ *       "company_id": "12345678",
  *       "company_name": "lego",
- *       "context": "suppliers"
+ *       "context": "suppliers",
+ *       "black_list" : "0",
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
  *     }
  */
 
 /**
- * @api {post} /buisness/change-status/:id Change Account Status
+ * @api {post} /buisness/change-status/:id 5.Change Account Status
  * @apiName ChangeAccountStatus
  * @apiVersion 1.0.0
  * @apiGroup Buisness Account
@@ -93,24 +108,29 @@
  * @apiSuccess {object[]} changed objects.
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
- *     "status":"success",
+ *     "status":"Account's statuses changed.",
  *     "message":
- *     [{
+ *     [
+ *      {
  *       "primary_id": "123",
  *       "type": "individual",
  *       "account_id": "1234",
  *       "currency": "USD"",
  *       "balance": 10024.3,
  *       "status": "ACTIVE",
- *       "address_id": "31",
+ *       "address": "",
  *       "company_id": "312",
  *       "company_name": "lego",
- *       "context": "suppliers"
- *     }]
+ *       "context": "suppliers",
+ *       "black_list" : "0",
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
+ *     }
+ * ]
  */
 
 /**
- * @api {post} /buisness/transfer/same-currency Trasnfer to Buisness/Individual with same currency
+ * @api {post} /buisness/transfer/same-currency 3.Trasnfer to Buisness/Individual with same currency
  * @apiName TrasnferSameCurrency
  * @apiVersion 1.0.0
  * @apiGroup Buisness Account
@@ -120,14 +140,46 @@
 
  * @apiSuccess {String} status status of the response.
  * @apiSuccess {string} message action outcome.
+ * @apiSuccess {object[]} changed objects.
+
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
- *     "status":"success",
- *     "message": "money transfered"
+ *     "status":200,
+ *     "message":"Transfer has done successfully",
+ *      "data": [
+        {
+ *       "type": "buisness",
+ *       "account_id": "1234567",
+ *       "currency": "USD"",
+ *       "balance": 1000,
+ *       "status": "ACTIVE",
+ *       "address": "",
+ *       "company_id": "312",
+ *       "company_name": "lego",
+ *       "context": "suppliers",
+ *       "black_list" : "0",
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
+ *     },
+        {
+ *       "type": "buisness",
+ *       "account_id": "1234568",
+ *       "currency": "USD"",
+ *       "balance": 3000,
+ *       "status": "ACTIVE",
+ *       "address": "",
+ *       "company_id": "12345678",
+ *       "company_name": "lego",
+ *       "context": "suppliers",
+ *       "black_list" : "0",
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
+ *     }
+    ]
  */
 
 /**
- * @api {post} /buisness/transfer/different-currency Trasnfer to Buisness with different currency
+ * @api {post} /buisness/transfer/different-currency 4.Trasnfer to Buisness with different currency
  * @apiName TrasnferDifferentCurrency
  * @apiVersion 1.0.0
  * @apiGroup Buisness Account
@@ -137,8 +189,39 @@
 
  * @apiSuccess {String} status status of the response.
  * @apiSuccess {string} message action outcome.
+ * @apiSuccess {object[]} changed objects.
+
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
- *     "status":"success",
- *     "message": "money transfered"
+ *     "message":"Transfer has done successfully",
+ *     "data": [
+        {
+ *       "type": "buisness",
+ *       "account_id": "1234567",
+ *       "currency": "USD"",
+ *       "balance": 1000,
+ *       "status": "ACTIVE",
+ *       "address": "",
+ *       "company_id": "312",
+ *       "company_name": "lego",
+ *       "context": "suppliers",
+ *       "black_list" : "0",
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
+ *     },
+        {
+ *       "type": "buisness",
+ *       "account_id": "1234568",
+ *       "currency": "USD"",
+ *       "balance": 3000,
+ *       "status": "ACTIVE",
+ *       "address": "",
+ *       "company_id": "12345678",
+ *       "company_name": "lego",
+ *       "context": "suppliers",
+ *       "black_list" : "0",
+ *       "e_date" : "2022-02-12T11:08:05.000Z",
+ *       "a_date" : "2022-02-12T11:08:05.000Z"
+ *     }
+    ]
  */
--- apidoc/src/family_routes.js
+++ apidoc/src/family_routes.js
@@ -1,5 +1,5 @@
 /**
- * @api {get} /family/:id/:details_level Request family account information
+ * @api {get} /family/:id/:details_level 1.Request family account information
  * @apiName GetFamilyAccountInformation
  * @apiVersion 1.0.0
  * @apiGroup Family Account
@@ -19,34 +19,49 @@
  *     }
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
- *     {
- *       "primary_id": "123",
- *       "type": "family",
- *       "currency": "USD"",
- *       "balance": 10024.3,
- *       "status": "ACTIVE",
- *       "address_id": "31",
- *       "context": "suppliers"
- *       "owners":[
- *       {
- *           "primary_id": "123",
- *           "type": "individual",
- *           "firstname": "John",
- *           "lastname": "Doe",
- *           "account_id": "1234",
- *           "currency": "USD"",
- *           "balance": 10024.3,
- *           "status": "INACTIVE",
- *           "address_id": "31",
- *           "individuel_id": "1234567",
- *           "email": "jeses.c@gmail.com"
- *          }
- *        ]
- *     }
+ {
+    "status": 200,
+    "message": "Family account found",
+    "data": {
+        "account_id": 218,
+        "currency": "EUR",
+        "balance": 24000,
+        "status_id": 1,
+        "a_date": "2022-02-12T10:13:41.000Z",
+        "e_date": "2022-02-12T10:15:13.000Z",
+        "context": null,
+        "owners": [
+            {
+                "account_id": 212,
+                "currency": "EUR",
+                "balance": 811036,
+                "status_id": 1,
+                "a_date": "2022-02-12T09:32:13.000Z",
+                "e_date": "2022-02-12T10:14:25.000Z",
+                "individual_id": 1000126,
+                "first_name": "omri7",
+                "last_name": "Cohen",
+                "email": "omri@gmail.com",
+                "address": {
+                    "address_id": 11,
+                    "country_name": "oklslsa",
+                    "country_code": null,
+                    "postal_code": null,
+                    "city": null,
+                    "region": null,
+                    "street_name": null,
+                    "street_number": null,
+                    "a_date": "2022-02-12T09:32:13.000Z",
+                    "e_date": "2022-02-12T09:32:13.000Z"
+                }
+            }
+        ]
+    }
+}
  */
 
 /**
- * @api {post} /family/ create family account information
+ * @api {post} /family/ 2.create family account information
  * @apiName CreateFamilyAccount
  * @apiVersion 1.0.0
  * @apiGroup Family Account
@@ -74,58 +89,55 @@
  *       "error": "Account Was Not Created
  *     }
  * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "message": "account created succesfuly",
- *       "primary_id": "123",
- *       "type": "individual",
- *       "account_id": "1234",
- *       "currency": "USD"",
- *       "balance": 10024.3,
- *       "status": "ACTIVE",
- *       "address_id": "31",
- *       "company_id": "312",
- *       "company_name": "lego",
- *       "context": "suppliers"
- *     }
+{
+    "status": 200,
+    "message": "Family account Created",
+    "data": {
+        "account_id": 218,
+        "currency": "EUR",
+        "balance": 24000,
+        "status_id": 1,
+        "a_date": "2022-02-12T10:13:41.000Z",
+        "e_date": "2022-02-12T10:15:13.000Z",
+        "context": null,
+        "owners": [
+            {
+                "account_id": 212,
+                "currency": "EUR",
+                "balance": 811036,
+                "status_id": 1,
+                "a_date": "2022-02-12T09:32:13.000Z",
+                "e_date": "2022-02-12T10:14:25.000Z",
+                "individual_id": 1000126,
+                "first_name": "omri7",
+                "last_name": "Cohen",
+                "email": "omri@gmail.com",
+                "address": {
+                    "address_id": 11,
+                    "country_name": "oklslsa",
+                    "country_code": null,
+                    "postal_code": null,
+                    "city": null,
+                    "region": null,
+                    "street_name": null,
+                    "street_number": null,
+                    "a_date": "2022-02-12T09:32:13.000Z",
+                    "e_date": "2022-02-12T09:32:13.000Z"
+                }
+            }
+        ]
+    }
+}
  */
 
 /**
- * @api {post} /family/change-status/:id Change Account Status
- * @apiName ChangeAccountStatus
- * @apiVersion 1.0.0
- * @apiGroup Family Account
- * @apiParam {string} new_status new_status.
- * @apiParam {string[]} ids ids to change status for.
- * @apiSuccess {String} status status of the response.
- * @apiSuccess {object[]} changed objects.
- * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     "status":"success",
- *     "message":
- *     [{
- *       "primary_id": "123",
- *       "type": "individual",
- *       "account_id": "1234",
- *       "currency": "USD"",
- *       "balance": 10024.3,
- *       "status": "ACTIVE",
- *       "address_id": "31",
- *       "company_id": "312",
- *       "company_name": "lego",
- *       "context": "suppliers"
- *     }]
- */
-
-/**
- * @api {post} /family/transfer/same-currency Trasnfer to Buisness with same currency
+ * @api {post} /family/transfer 3.Trasnfer to Buisness with same currency
  * @apiName TrasnferSameCurrencyFamily
  * @apiVersion 1.0.0
  * @apiGroup Family Account
  * @apiParam {number} source source account primary id.
  * @apiParam {number} destination destination account primary id.
  * @apiParam {number} amount amount to transfer.
-
  * @apiSuccess {String} status status of the response.
  * @apiSuccess {string} message action outcome.
  * @apiSuccessExample {json} Success-Response:
@@ -135,7 +147,7 @@
  */
 
  /**
- * @api {post} /family/close/:id Close Account 
+ * @api {post} /family/close/:id 6.Close Account 
  * @apiName CloseAccount 
  * @apiVersion 1.0.0
  * @apiGroup Family Account
@@ -148,8 +160,8 @@
  */
 
  /**
- * @api {post} family/:family_id/:details_level add individual to family
- * @apiName AdsIndividualToFamily
+ * @api {post} family/add/:family_id/:details_level 4.add individual to family
+ * @apiName AddIndividualToFamily
  * @apiVersion 1.0.0
  * @apiGroup Family Account
  * @apiParam {string} new_status new_status.
@@ -157,19 +169,143 @@
  * @apiSuccess {String} status status of the response.
  * @apiSuccess {object[]} changed objects.
  * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     "status":"success",
- *     "message":
- *     [{
- *       "primary_id": "123",
- *       "type": "individual",
- *       "account_id": "1234",
- *       "currency": "USD"",
- *       "balance": 10024.3,
- *       "status": "ACTIVE",
- *       "address_id": "31",
- *       "company_id": "312",
- *       "company_name": "lego",
- *       "context": "suppliers"
- *     }]
+ {
+    "status": 200,
+    "message": "Family account found",
+    "data": {
+        "account_id": 218,
+        "currency": "EUR",
+        "balance": 24000,
+        "status_id": 1,
+        "a_date": "2022-02-12T10:13:41.000Z",
+        "e_date": "2022-02-12T10:15:13.000Z",
+        "context": null,
+        "owners": [
+            {
+                "account_id": 212,
+                "currency": "EUR",
+                "balance": 811036,
+                "status_id": 1,
+                "a_date": "2022-02-12T09:32:13.000Z",
+                "e_date": "2022-02-12T10:14:25.000Z",
+                "individual_id": 1000126,
+                "first_name": "omri7",
+                "last_name": "Cohen",
+                "email": "omri@gmail.com",
+                "address": {
+                    "address_id": 11,
+                    "country_name": "oklslsa",
+                    "country_code": null,
+                    "postal_code": null,
+                    "city": null,
+                    "region": null,
+                    "street_name": null,
+                    "street_number": null,
+                    "a_date": "2022-02-12T09:32:13.000Z",
+                    "e_date": "2022-02-12T09:32:13.000Z"
+                }
+            },
+            {
+                "account_id": 216,
+                "currency": "EUR",
+                "balance": 891009,
+                "status_id": 1,
+                "a_date": "2022-02-12T09:48:51.000Z",
+                "e_date": "2022-02-12T10:15:13.000Z",
+                "individual_id": 1000127,
+                "first_name": "omri7",
+                "last_name": "Cohen",
+                "email": "omri@gmail.com",
+                "address": {
+                    "address_id": 12,
+                    "country_name": "oklslsa",
+                    "country_code": null,
+                    "postal_code": null,
+                    "city": null,
+                    "region": null,
+                    "street_name": null,
+                    "street_number": null,
+                    "a_date": "2022-02-12T09:48:51.000Z",
+                    "e_date": "2022-02-12T09:48:51.000Z"
+                }
+            }
+        ]
+    }
+}
+ */
+
+
+ /**
+ * @api {post} family/remove/:family_id/:details_level 5. remove individual to family
+ * @apiName RemoveIndividualFromFamily
+ * @apiVersion 1.0.0
+ * @apiGroup Family Account
+ * @apiParam {string} new_status new_status.
+ * @apiParam {string[]} ids ids to change status for.
+ * @apiSuccess {String} status status of the response.
+ * @apiSuccess {object[]} changed objects.
+ * @apiSuccessExample {json} Success-Response:
+ {
+    "status": 200,
+    "message": "Family account found",
+    "data": {
+        "account_id": 218,
+        "currency": "EUR",
+        "balance": 24000,
+        "status_id": 1,
+        "a_date": "2022-02-12T10:13:41.000Z",
+        "e_date": "2022-02-12T10:15:13.000Z",
+        "context": null,
+        "owners": [
+            {
+                "account_id": 212,
+                "currency": "EUR",
+                "balance": 811036,
+                "status_id": 1,
+                "a_date": "2022-02-12T09:32:13.000Z",
+                "e_date": "2022-02-12T10:14:25.000Z",
+                "individual_id": 1000126,
+                "first_name": "omri7",
+                "last_name": "Cohen",
+                "email": "omri@gmail.com",
+                "address": {
+                    "address_id": 11,
+                    "country_name": "oklslsa",
+                    "country_code": null,
+                    "postal_code": null,
+                    "city": null,
+                    "region": null,
+                    "street_name": null,
+                    "street_number": null,
+                    "a_date": "2022-02-12T09:32:13.000Z",
+                    "e_date": "2022-02-12T09:32:13.000Z"
+                }
+            },
+            {
+                "account_id": 216,
+                "currency": "EUR",
+                "balance": 891009,
+                "status_id": 1,
+                "a_date": "2022-02-12T09:48:51.000Z",
+                "e_date": "2022-02-12T10:15:13.000Z",
+                "individual_id": 1000127,
+                "first_name": "omri7",
+                "last_name": "Cohen",
+                "email": "omri@gmail.com",
+                "address": {
+                    "address_id": 12,
+                    "country_name": "oklslsa",
+                    "country_code": null,
+                    "postal_code": null,
+                    "city": null,
+                    "region": null,
+                    "street_name": null,
+                    "street_number": null,
+                    "a_date": "2022-02-12T09:48:51.000Z",
+                    "e_date": "2022-02-12T09:48:51.000Z"
+                }
+            }
+        ]
+    }
+}
  */
